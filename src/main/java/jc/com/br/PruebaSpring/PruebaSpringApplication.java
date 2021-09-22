c4e0015fdbd16d9eeb30448d03e8a06a757bdaf5
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-21 20:29:56 -0500

    Se sube el proyecto base en maven de SpringBoot

---
+++ src/main/java/jc/com/br/PruebaSpring/PruebaSpringApplication.java
@@ -0,0 +1,13 @@
+package jc.com.br.PruebaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSpringApplication.class, args);
+	}
+
+}

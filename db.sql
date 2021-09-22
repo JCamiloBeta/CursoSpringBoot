e6db6b848d49d2a202328586e5df0033a9b35667
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-22 16:59:28 -0500

    Se añadió el CREATE y la página para crear datos de persona.

--- db.sql
+++ db.sql
@@ -12,3 +12,5 @@ INSERT INTO `test`.`persona` (`nombre`, `apellido`, `email`, `telefono`) VALUES 
 INSERT INTO `test`.`persona` (`nombre`, `apellido`, `email`, `telefono`) VALUES ('Camilo', 'Rico', 'camilo@mail.com', '6544789');
 INSERT INTO `test`.`persona` (`nombre`, `apellido`, `email`, `telefono`) VALUES ('JC', 'BR', 'jcbr@email.com', '477852');
 INSERT INTO `test`.`persona` (`nombre`, `apellido`, `email`, `telefono`) VALUES ('Carola', 'Restrepo', 'cr@gmail.com', '789511');
+--Para reiniciar el autorincrementable del id de la tabla
+ALTER TABLE persona AUTO_INCREMENT = 1;
--- src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
+++ src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
@@ -1,6 +1,7 @@
 package jc.com.br.PruebaSpring.web;
 
 
+import jc.com.br.PruebaSpring.domain.Persona;
 import jc.com.br.PruebaSpring.servicio.IPersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /*Anotación para que reconozca esta clase como controlador de Spring*/
 @Controller
@@ -36,4 +38,14 @@ public class ControladorInicio{
         return "index";
     }
 
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
 }

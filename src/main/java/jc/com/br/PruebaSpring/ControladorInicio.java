cd1ff6bed2b26e397cda87cd11371cb52d0de7b2
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-21 23:36:44 -0500

    Creaciòn de una lista Personas y como Thymeleaf lo trabaja.

--- src/main/java/jc/com/br/PruebaSpring/ControladorInicio.java
+++ src/main/java/jc/com/br/PruebaSpring/ControladorInicio.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*Anotación para que reconozca esta clase como controlador de Spring*/
 @Controller
 /*Para el manejo del Login*/
@@ -20,17 +23,31 @@ public class ControladorInicio{
     public String inicio(Model model){
         var mensaje = "Hola mundo con Thymeleaf";
         var persona = new Persona();
+        var persona2 = new Persona();
+        var persona3 = new Persona();
 
         persona.setNombre("Camilo");
         persona.setApellido("Betancur");
         persona.setEmail("jbetancurricogmail.com");
         persona.setTelefono("3105149288");
 
+        persona2.setNombre("Juan");
+        persona2.setApellido("Rico");
+        persona2.setEmail("jbetancurrico@gmail.com");
+        persona2.setTelefono("3022233");
+
+        persona3.setNombre("JC");
+        persona3.setApellido("BR");
+        persona3.setEmail("jcamilob@outlook.com");
+        persona3.setTelefono("3206905927");
+
+        var personas = Arrays.asList(persona, persona2, persona3);
+
         log.info("Ejecutando el controlador Spring MVC");
 
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
-        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 

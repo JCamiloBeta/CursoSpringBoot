1379d04dca54134bf00a71d9b6608fa5cf5378bc
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-21 22:02:07 -0500

    Compartir información con Spring MVC y

--- src/main/java/jc/com/br/PruebaSpring/ControladorInicio.java
+++ src/main/java/jc/com/br/PruebaSpring/ControladorInicio.java
@@ -1,18 +1,28 @@
 package jc.com.br.PruebaSpring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*Anotación para que reconozca esta clase como controlador de Spring*/
 @Controller
 /*Para el manejo del Login*/
 @Slf4j
-public class ControladorInicio {
+public class ControladorInicio{
+
+    @Value("${index.saludo}")
+    private String saludo;
 
     @GetMapping("/")
-    public String inicio(){
+    public String inicio(Model model){
+        var mensaje = "Hola mundo con Thymeleaf";
+
         log.info("Ejecutando el controlador Spring MVC");
+
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
         return "index";
     }
 

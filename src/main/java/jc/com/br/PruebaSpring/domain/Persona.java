017ed2296cf09f80784c1af2ac1efb4a616a7207
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-23 09:52:40 -0500

    Se agregaron validaciones a los campos de Persona

--- src/main/java/jc/com/br/PruebaSpring/domain/Persona.java
+++ src/main/java/jc/com/br/PruebaSpring/domain/Persona.java
@@ -3,7 +3,12 @@ package jc.com.br.PruebaSpring.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
+
 
 @Data
 @Entity
@@ -15,8 +20,15 @@ public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_persona;
+
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
+    @Email
     private String email;
+    @Size(min = 7, max = 15)
+    @Digits(fraction = 0, integer = 10)
     private String telefono;
 }
--- src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
+++ src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /*Anotación para que reconozca esta clase como controlador de Spring*/
 @Controller
@@ -44,7 +47,10 @@ public class ControladorInicio{
     }
 
     @PostMapping("/guardar")
-    public String guardar(Persona persona){
+    public String guardar(@Valid Persona persona, Errors errors){
+        if(errors.hasErrors()){
+            return "modificar";
+        }
         personaService.guardar(persona);
         return "redirect:/";
     }

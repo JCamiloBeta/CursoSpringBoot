853261392ee0719250c3dd766e45e5a8dfe7edd2
JCamiloBeta <jbetancurrico@gmail.com>
2021-09-23 16:34:36 -0500

    Se restringen los accesos según el rol del usuario

--- src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
+++ src/main/java/jc/com/br/PruebaSpring/web/ControladorInicio.java
@@ -6,6 +6,8 @@ import jc.com.br.PruebaSpring.servicio.IPersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -29,10 +31,11 @@ public class ControladorInicio{
     private String saludo;
 
     @GetMapping("/")
-    public String inicio(Model model){
+    public String inicio(Model model, @AuthenticationPrincipal User user){
         var mensaje = "Hola mundo con Thymeleaf";
         var personas = personaService.listarPersona();
 
+        log.info("Usuario que hizo login:" + user);
         log.info("Ejecutando el controlador Spring MVC");
 
         model.addAttribute("mensaje", mensaje);
--- src/main/java/jc/com/br/PruebaSpring/web/SecurityConfig.java
+++ src/main/java/jc/com/br/PruebaSpring/web/SecurityConfig.java
@@ -3,6 +3,7 @@ package jc.com.br.PruebaSpring.web;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,5 +21,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("usuario")
                     .password("{noop}1997")
                     .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/editar/**", "/agregar/**", "/eliminar")
+                    .hasRole("ADMIN")
+                .antMatchers("/")
+                    .hasAnyRole("USER", "ADMIN")
+        .and()
+            .formLogin()
+            .loginPage("/login")
+        .and()
+            .exceptionHandling().accessDeniedPage("/errores/403");
     }
 }
